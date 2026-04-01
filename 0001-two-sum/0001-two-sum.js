684a6dac97bbe44cd05abc38603d9d8fb521d1ba
Moges Aklilu <eyobaklilu175@gmail.com>
2026-04-01 15:09:51 -0700

    Time: 1 ms (86.58%), Space: 53.9 MB (92.79%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+// var twoSum = function(nums, target) {
+    
+// };
+
+function twoSum(nums, target) {
+    const map = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+
+        if (map.hasOwnProperty(complement)) {
+            return [map[complement], i];
+        }
+
+        map[nums[i]] = i;
+    }
+}
